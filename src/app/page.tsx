9503f3eac140f20c2003091b246bb0ee3a3c3a66
Pedro Paulo Cantalice Marinho Braga <pedro.cantalice@assert.ifpb.edu.br>
2023-09-21 15:40:52 -0300

    Second Study Pt1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import { GeoForm } from "@/components/GeoForm";
 import { Person } from "@/components/Person";
+import { Card } from "@/components/Card";
+import { Square } from "@/components/Square";
 
 const Page = () => {
   return (
   <div>
-    <Person/>
-    <GeoForm/>
+    <h1 className="font-bold text-2xl">Olá Mundo</h1>
+    <h3>Algum outro texto</h3>
+    
+    <Card phrase="Alguma frase TOP" author="Coach de milhões"/>
   </div>
   );
 }
