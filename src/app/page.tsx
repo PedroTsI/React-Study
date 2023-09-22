2808a048a9d4047a5c9826fa3fcb4c46ed4693fb
Pedro Paulo Cantalice Marinho Braga <pedro.cantalice@assert.ifpb.edu.br>
2023-09-21 23:01:09 -0300

    emoji rating activity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,9 @@
+import { EmojiRating } from "@/components/EmojiRating";
 
 const Page = () => {
-  const fullTime = new Intl.DateTimeFormat('pt-BR', {
-    timeStyle: 'short',
-    hour12: false
-  }).format();
-
-  const hour = new Date().getHours();
-  let greeting = '';
-
-  if(hour >= 0 && hour < 12) {
-    greeting = 'Bom dia 😁';
-  } else if(hour >= 12 && hour < 18) {
-    greeting = 'Boa tarde 🤓';
-  } else {
-    greeting = 'Boa noite 😴';
-  }
-
   return (
   <div className="w-screen h-screen flex flex-col justify-center items-center text-white bg-gradient-to-r from-sky-500 to-indigo-500">
-    <div className="text-9xl">{fullTime}</div>
-    <div className="text-5xl font-bold">{greeting}</div>
+    <EmojiRating rating={4.2} />
   </div>
   );
 }
