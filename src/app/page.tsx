b688ca9b96fe4284edfe73df3bf545c325f56d6c
PedroTsI <cantalice.pedro@academico.ifpb.edu.br>
2024-09-24 20:39:29 -0300

    Att Main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,5 @@
 "use client"
 
-import { FormEvent } from "react";
-
-const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
-  e.preventDefault();
-  alert("Oi Catrina");
-}
-
 const Page = () => {
     return (
         <div className="w-screen h-screen flex flex-col justify-center items-center text-3xl">
